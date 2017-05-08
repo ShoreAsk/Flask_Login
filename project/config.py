9abb94eca5b14aec01e95f7440e47f5e47359244
Hitesh Gupta T R <hitesh.2457@outlook.com>
2017-05-08 23:09:51 +0530

    updating the initial api set

--- project/config.py
+++ project/config.py
@@ -8,5 +8,5 @@ class BaseConfig(object):
     SECRET_KEY = 'my_precious'
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 13
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost:32771/shoretel'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://pyflask:pyflask:@localhost:5432/shoretel'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
